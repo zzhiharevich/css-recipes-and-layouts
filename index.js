39e6947c9ad4e1365e8bbd4902e3fa189fa56ee1
Aleksandra <izhukharevich@icloud.com>
2019-07-25 13:26:35 +0300

    add localhost

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
