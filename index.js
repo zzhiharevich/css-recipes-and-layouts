e7c1524949c2d6bb8ef1a1c2e9928dee3102e63a
Aleksandra <izhukharevich@icloud.com>
2019-07-26 20:14:41 +0300

    modified

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-})
+app.use(express.static('public'))
 app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
